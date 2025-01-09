ce1ead5f3157303163652a263e5ad090d53076e5
Shubhra Agarwal <shubhra.agarwal@udaan.com>
2025-01-09 15:52:55 +0530

    added testfixtures for rename-unsafe-lifecycles

--- codemods/react/rename-unsafe-lifecycles/__testfixtures__/fixture1.input.ts
+++ codemods/react/rename-unsafe-lifecycles/__testfixtures__/fixture1.input.ts
@@ -1,1 +1,22 @@
-const toReplace = "hello";
+import React from 'react';
+
+class MyComponent extends React.Component {
+    componentWillMount() {
+        console.log('component will mount');
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('component will receive props', nextProps);
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('component will update', nextProps, nextState);
+    }
+
+    render() {
+        return <div>My Component</div>;
+    }
+}
+
+
+export { MyComponent };
--- codemods/react/rename-unsafe-lifecycles/__testfixtures__/fixture1.output.ts
+++ codemods/react/rename-unsafe-lifecycles/__testfixtures__/fixture1.output.ts
@@ -1,1 +1,25 @@
-const replacement = "hello";
+import React from 'react';
+
+class MyComponent extends React.Component {
+    UNSAFE_componentWillMount() {
+        // Migration: renamed from componentWillMount
+        console.log('Component will mount');
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        // Migration: renamed from componentWillReceiveProps
+            console.log('Props will update');
+
+    }
+
+    UNSAFE_componentWillUpdate(nextProps, nextState) {
+        // Migration: renamed from componentWillUpdate
+        console.log('Component will update');
+    }
+
+    render() {
+        return <div>Test Component</div>;
+    }
+}
+
+export default MyComponent;
