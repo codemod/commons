3addfdd897b27c6a264a3b6a8d2925a3b0903e8c
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-01-19 13:26:25 +0330

    fix: fix test fixtures in codemods/msw/2/response-usages

--- codemods/msw/2/response-usages/__testfixtures__/fixture2.output.ts
+++ codemods/msw/2/response-usages/__testfixtures__/fixture2.output.ts
@@ -4,7 +4,7 @@ import { http, HttpResponse } from "msw";
 http.get("/resource", (req, res, ctx) => {
   return HttpResponse.json(null, {
     headers: {
-      "Set-Cookie": "token=abc-123;",
-    },
+      "Set-Cookie": "token=abc-123;"
+    }
   });
 });
